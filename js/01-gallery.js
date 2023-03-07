a5f1892b4619086a261861edecbd57610360b929
Oleksy Lykov <lukov_jr@ukr.net>
2023-03-07 11:18:13 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(galleryItems) {
+	return galleryItems
+		.map(
+			({ preview, description, original }) => `
+				<div class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+						/>
+					</a>
+				</div>
+        	`,
+		)
+		.join("");
+}
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(
+		`
+		<img src="${event.target.dataset.source}">
+	`,
+	);
+
+	instance.show();
+
+	galleryEl.addEventListener("keydown", onEscKeyDown);
+
+	function onEscKeyDown(event) {
+		if (event.code !== "Escape") {
+			return;
+		}
+		instance.close();
+		galleryEl.removeEventListener("keydown", onEscKeyDown);
+	}
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(galleryItems) {
+	return galleryItems
+		.map(
+			({ preview, description, original }) => `
+				<div class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+						/>
+					</a>
+				</div>
+        	`,
+		)
+		.join("");
+}
+
+galleryEl.addEventListener("click", lightbox);
+
+var lightbox = new SimpleLightbox(".gallery .gallery__link", {
+	captionsData: "alt",
+	captionDelay: 250,
+	scrollZoom: false,
+});
